f2fb27350da82b89831129a546845fa9ff50834e
Nadim Mabrouk <mabrouknadim2@gmail.com>
2018-10-10 14:34:50 +0200

    first commit

---
+++ src/History.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class History extends Component{
+    render(){
+        return(
+            <div>
+                <p>Notres Histoire</p>
+            </div>
+        )
+    }
+}
+
+export default History
